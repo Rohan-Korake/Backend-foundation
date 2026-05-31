a07e48254c432968211436519c465bc58dbe984d
Rohan Korake <rohannkorake@gmail.com>
2026-05-31 23:02:46 +0530

    refactor: simplify user registration validation and improve error handling in email sending

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -11,6 +11,7 @@ import Mailgen from "mailgen";
 import jwt from "jsonwebtoken";
 import { response } from "express";
 import crypto from "crypto";
+import { log } from "console";
 
 const generateAccessAndRefreshToken = async (userId) => {
   try {
@@ -35,13 +36,13 @@ const registerUser = asyncHandler(async (req, res) => {
   const { email, username, password } = req.body;
 
   // Check if a user already exists with the same username OR email
-  const existedUser = await User.findOne({
-    $or: [{ username }, { email }],
-  });
+  const existedUser = await User.findOne({ email });
 
-  // send error resposne
+  // send 409 resposne
   if (existedUser) {
-    throw new apiError(409, "Email is already exists");
+    return res
+      .status(409)
+      .json(new apiResponse(409, {}, "Email is already exists"));
   }
 
   // create user
@@ -79,15 +80,16 @@ const registerUser = asyncHandler(async (req, res) => {
 
   //   handle error
   if (!createdUser) {
-    throw new apiError(500, "Something went wrong while registering the user");
+    return res
+      .status(500)
+      .json(new apiResponse(500, {}, "Something went wrong"));
   }
 
   //   respose back to request
   return res.status(200).json(
     new apiResponse(200, {
       user: createdUser,
-      message:
-        "User registered successfully and verification email has sent on your email",
+      message: "User registered successfully. Verification email sent.",
     }),
   );
 });
@@ -111,6 +113,34 @@ const loginUser = asyncHandler(async (req, res) => {
   const isPasswordValid = await user.isPasswordCorrect(password);
   if (!isPasswordValid) {
     res.status(401).json(new apiResponse(401, {}, "Invalid credentails"));
+  }
+
+  // check email is verified or not before login and resend email
+  const emailVerified = await user.isEmailVerified;
+  if (!emailVerified) {
+    // Generate tokens
+    const { unHashedToken, hashedToken, tokenExpiry } =
+      user.generateTemporaryToken();
+
+    user.emailVerificationToken = hashedToken;
+    user.emailVerificationExpiry = tokenExpiry;
+
+    //   save the data in DB
+    await user.save({ validateBeforeSave: false });
+
+    //   send email to user
+    await sendEmail({
+      email: user.email,
+      subject: "Please verify your email",
+      mailGenContent: emailVerificationMailContent(
+        user.username,
+        `${req.protocol}://${req.get("host")}/verify-email/${unHashedToken}`,
+      ),
+    });
+
+    return res
+      .status(403)
+      .json(new apiResponse(403, {}, "Please Verify email"));
   }
 
   // once the credentails are valid generate tokens
@@ -341,7 +371,9 @@ const forgotPassword = asyncHandler(async (req, res) => {
 
   // handle error if user not found
   if (!user) {
-    throw new apiError(404, "User does not exists");
+    return res
+      .status(404)
+      .json(new apiResponse(404, {}, "User does not exists"));
   }
 
   // Generate tokens
--- src/models/user.models.js
+++ src/models/user.models.js
@@ -19,20 +19,14 @@ const userSchema = new Schema(
     username: {
       type: String,
       required: true,
-      unique: true,
       lowercase: true,
       trim: true,
-      index: true,
     },
     email: {
       type: String,
       required: true,
       unique: true,
       lowercase: true,
-      trim: true,
-    },
-    fullName: {
-      type: String,
       trim: true,
     },
     password: {
